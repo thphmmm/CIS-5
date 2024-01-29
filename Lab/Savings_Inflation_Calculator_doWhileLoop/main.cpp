5ed94b1f8dc1ec21e955d96783bf702191b93993
Thu Pham <143148714+thphmmm@users.noreply.github.com>
2024-01-28 22:36:33 -0800

    Add files via upload

---
+++ Lab/Savings_Inflation_Calculator_doWhileLoop/main.cpp
@@ -0,0 +1,34 @@
+/*
+/* 
+ * File:   main.cpp
+ * Author: Thu Pham
+ * Created on January 2, 2024, 5:48 PM
+ * Purpose: Template used to start all projects
+ */
+
+//System Libraries
+#include <iostream> //Input-Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math,Physics,Chemistry,Conversions
+
+//Function Prototypes
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Set a random seed
+    
+    //Declare all variables
+    
+    //Initialize all variables
+    
+    //Process or Map solutions
+    
+    //Display the output
+    
+    //Exit the Program
+    return 0;
+}
+
