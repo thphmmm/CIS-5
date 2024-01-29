d17aaf20b14798c440070d12c328d66c7a20b674
Thu Pham <143148714+thphmmm@users.noreply.github.com>
2024-01-28 21:56:34 -0800

    Add files via upload

---
+++ Class/main.cpp
@@ -0,0 +1,39 @@
+/*
+/* 
+ * File:   main.cpp
+ * Author: Thu Pham
+ * Created on January 2, 2024, 5:48 PM
+ * Purpose: Illustrates Computer Errors
+ */
+
+//System Libraries
+#include <iostream> //Input-Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math,Physics.Chemistry.Conversions
+
+//Function Prototypes
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Set a random seed
+    
+    //Declare all variables
+    short a,b,c;
+    
+    //Initialize all variables
+    a=10000;
+    b=30000;
+    
+    //Process or Map solutions
+    c=a+b;
+    
+    //Display the output
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    
+    //Exit the Program
+    return 0;
+}
+
