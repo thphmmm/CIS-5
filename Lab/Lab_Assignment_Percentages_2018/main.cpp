65e77407cf43cc464db77871c6371a2376a7394b
Thu Pham <143148714+thphmmm@users.noreply.github.com>
2024-01-28 22:26:27 -0800

    Add files via upload

---
+++ Lab/Lab_Assignment_Percentages_2018/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: Thu Pham
+ * Created on January 11, 2024, 11:02 PM
+ * Purpose: Calculate Military Budget
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+#include <iomanip>   //Format Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+
+
+//Declare Variables
+    float milBdgt,//Military Budget
+          fedBdgt,//Federal Budget
+          milPer;//Military budget as a percentage of the federal budget
+    
+    //Initialize or input i.e. set variable values
+          milBdgt= 7.4e11f,//In 740 billion $'s
+          fedBdgt=6.8e12f;//In 6.8 trillion $'s
+    
+    //Map inputs -> outputs
+          milPer=milBdgt/fedBdgt*100;
+
+    //Display the outputs
+        cout<<fixed<<setprecision(1)<<showpoint;
+        cout<<"Military Budget Percentage : "<<milPer<<" %"<<endl;
+          
+    //Exit stage right or left!
+    return 0;
+}
