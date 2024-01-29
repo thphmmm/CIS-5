060006275416948a4e623318a357e5731cb65b6e
Thu Pham <143148714+thphmmm@users.noreply.github.com>
2024-01-28 22:04:47 -0800

    Add files via upload

---
+++ Class/TotalPaySumed_ToAFile/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=totalpaysumed_toafile
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/totalpaysumed_toafile
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=totalpaysumedtoafile.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/totalpaysumedtoafile.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=totalpaysumed_toafile
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/totalpaysumed_toafile
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=totalpaysumedtoafile.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/totalpaysumedtoafile.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
